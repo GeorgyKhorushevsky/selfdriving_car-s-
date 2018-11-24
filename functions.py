e6ddfe7ef2b4460bb912d0ca03a5f13ef5a2bc54
GeorgyKhorushevsky <9067311794@mail.ru>
2018-11-24 05:09:24 +0300

    ист редач

--- functions.py
+++ functions.py
@@ -10,25 +10,26 @@ def two(date):
     #date is int array [mm, dd, yy]
     print(date)
     return []
-def three(self):
+def three():
     return []
-def four(self):
+def four():
     #some shit we don't know what to do
     return []
-def five(self):
+def five(date):
     return []
-def six(self):
+def six():
     #first array - 3 most popular locations, second - destinations
     return [[], []]
-def seven(self):
+def seven():
+    #list of 10% worst cars, as i understand
     return []
-def eight(self):
+def eight(date):
     #make it in couples
     return [[], [], []]
-def nine(self):
+def nine():
     #no idea what should be in the output
     return []
-def ten(self):
+def ten():
     #return 1 string - cartype.
     return "Volvo"
 
